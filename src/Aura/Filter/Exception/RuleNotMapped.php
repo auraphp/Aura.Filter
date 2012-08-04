daac181d291b06a868be47aaaa6052304c51593c
Hari K T <kthari85@gmail.com>
2012-08-04 17:09:15 +0530

    Added exceptions which are missed, travis and return to the sanitize

---
+++ src/Aura/Filter/Exception/RuleNotMapped.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * 
+ * This file is part of the Aura project for PHP.
+ * 
+ * @package Aura.Autoload
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
+namespace Aura\Filter\Exception;
+
+use Aura\Filter\Exception;
+/**
+ * 
+ * Rule not mapped exception.
+ * 
+ * @package Aura.Filter
+ * 
+ */
+class RuleNotMapped extends Exception
+{
+}
+ 
