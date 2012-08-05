489b43790eb5b3c3bcd5737a0468d4eecb75bbf7
Hari K T <kthari85@gmail.com>
2012-08-05 09:01:06 +0530

    Instance script and update example

--- scripts/instance.php
+++ scripts/instance.php
@@ -1,4 +1,38 @@
 <?php
 namespace Aura\Filter;
 require_once dirname(__DIR__) . '/src.php';
-return new ???;
+$registry = [
+    'alnum' => new Rule\Alnum,
+    'alpha' => new Rule\Rule\Alpha,
+    'between' => new Rule\Rule\Between,
+    'blank' => new Rule\Rule\Blank,
+    'bool' => new Rule\Bool,
+    'countryCode' => new Rule\CountryCode,
+    'creditCard' => new Rule\CreditCard,
+    'email' => new Rule\Email,
+    'equalToField' => new Rule\EqualToField,
+    'equalToValue' => new Rule\EqualToValue,
+    'float' => new Rule\Float,
+    'inKeys' => new Rule\InKeys,
+    'int' => new Rule\Int,
+    'inValues' => new Rule\InValues,
+    'ipv4' => new Rule\Ipv4,
+    'isoDate' => new Rule\IsoDate,
+    'isoTime' => new Rule\IsoTime,
+    'isoTimestamp' => new Rule\IsoTimestamp,
+    'max' => new Rule\Max,
+    'min' => new Rule\Min,
+    'regex' => new Rule\Regex,
+    'strictEqualToField' => new Rule\StrictEqualToField,
+    'strictEqualToValue' => new Rule\StrictEqualToValue,
+    'string' => new Rule\String,
+    'strlenBetween' => new Rule\StrlenBetween,
+    'strlenMax' => new Rule\StrlenMax,
+    'strlenMin' => new Rule\StrlenMin,
+    'strlen' => new Rule\Strlen,
+    'trim' => new Rule\Trim,
+    'upload' => new Rule\Upload,
+    'url' => new Rule\Url,
+    'word' => new Rule\Word
+];
+return new Chain(new RuleLocator($registry));
