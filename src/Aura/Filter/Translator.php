3e0e1c2843fab3ef91f31234f88a04a0de7e63e1
Paul M. Jones <pmjones88@gmail.com>
2013-01-22 15:51:07 -0600

    add translation facility and internationalization messages

---
+++ src/Aura/Filter/Translator.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * 
+ * This file is part of the Aura Project for PHP.
+ * 
+ * @package Aura.Cli
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
+namespace Aura\Filter;
+
+/**
+ * 
+ * Translator to translate the message
+ * 
+ * @package Aura.Cli
+ * 
+ */
+class Translator implements TranslatorInterface
+{
+    /**
+     * 
+     * The array of message keys and translations.
+     * 
+     * @var array
+     * 
+     */
+    protected $messages = [];
+
+    /**
+     * 
+     * Constructor.
+     * 
+     * @param array $package The package internationalization values.
+     * 
+     */
+    public function __construct(array $package)
+    {
+        $this->messages = $package['messages'];
+    }
+
+    /**
+     * 
+     * Translate the key with the token values replaced.
+     * 
+     * @param string $key The message key.
+     * 
+     * @param array $tokens_values The message placeholder tokens and their
+     * replacement values.
+     * 
+     * @return string The translated string.
+     * 
+     */
+    public function translate($key, array $tokens_values = [])
+    {
+        // retain the message string
+        $message = $this->messages[$key];
+
+        // are there token replacement values?
+        if (! $tokens_values) {
+            // no, return the message string as-is
+            return $message;
+        }
+
+        // do string replacements
+        foreach ($tokens_values as $token => $value) {
+            $message = str_replace('{' . $token . '}', $value, $message);
+        }
+        
+        // done!
+        return $message;
+    }
+}
