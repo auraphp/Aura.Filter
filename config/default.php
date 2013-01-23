a79dced09f808b64e737a67daef5026c4dee109d
Paul M. Jones <pmjones88@gmail.com>
2013-01-23 10:57:49 -0600

    update DI configs

--- config/default.php
+++ config/default.php
@@ -4,8 +4,29 @@
  */
 $loader->add('Aura\Filter\\', dirname(__DIR__) . DIRECTORY_SEPARATOR . 'src');
 
+// RuleCollection
+$di->params['Aura\Filter\RuleCollection']['rule_locator'] = $di->lazyNew('Aura\Filter\RuleLocator');
+$di->params['Aura\Filter\RuleCollection']['translator'] = $di->lazy(function () use ($di) {
+    $translators = $di->get('intl_translator_locator');
+    return $translators->get('Aura.Cli');
+});
+
 /**
- * Load rules
+ * Intl
+ */
+$di->params['Aura\Intl\PackageLocator']['registry']['Aura.Cli'] = [
+    'en_US' => function () use ($system) {
+        $package = require "$system/package/Aura.Cli/intl/en_US.php";
+        return new Aura\Intl\Package(
+            $package['formatter'],
+            $package['fallback'],
+            $package['messages']
+        );
+    },
+];
+
+/**
+ * Rules for the locator
  */
 $di->params['Aura\Filter\RuleLocator']['registry'] = [
     'alnum'                 => function() { return new \Aura\Filter\Rule\Alnum; },
@@ -24,8 +45,8 @@ $di->params['Aura\Filter\RuleLocator']['registry'] = [
     'equalToValue'          => function() { return new \Aura\Filter\Rule\EqualToValue; },
     'float'                 => function() { return new \Aura\Filter\Rule\Float; },
     'inKeys'                => function() { return new \Aura\Filter\Rule\InKeys; },
-    'int'                   => function() { return new \Aura\Filter\Rule\Int; },
     'inValues'              => function() { return new \Aura\Filter\Rule\InValues; },
+    'int'                   => function() { return new \Aura\Filter\Rule\Int; },
     'ipv4'                  => function() { return new \Aura\Filter\Rule\Ipv4; },
     'max'                   => function() { return new \Aura\Filter\Rule\Max; },
     'min'                   => function() { return new \Aura\Filter\Rule\Min; },
@@ -33,10 +54,10 @@ $di->params['Aura\Filter\RuleLocator']['registry'] = [
     'strictEqualToField'    => function() { return new \Aura\Filter\Rule\StrictEqualToField; },
     'strictEqualToValue'    => function() { return new \Aura\Filter\Rule\StrictEqualToValue; },
     'string'                => function() { return new \Aura\Filter\Rule\String; },
+    'strlen'                => function() { return new \Aura\Filter\Rule\Strlen; },
     'strlenBetween'         => function() { return new \Aura\Filter\Rule\StrlenBetween; },
     'strlenMax'             => function() { return new \Aura\Filter\Rule\StrlenMax; },
     'strlenMin'             => function() { return new \Aura\Filter\Rule\StrlenMin; },
-    'strlen'                => function() { return new \Aura\Filter\Rule\Strlen; },
     'trim'                  => function() { return new \Aura\Filter\Rule\Trim; },
     'upload'                => function() { return new \Aura\Filter\Rule\Upload; },
     'url'                   => function() { return new \Aura\Filter\Rule\Url; },
