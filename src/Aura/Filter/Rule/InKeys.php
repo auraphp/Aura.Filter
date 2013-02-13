0da7ed8518a32017b9617626aef6ff70a7f0df88
Paul M. Jones <pmjones88@gmail.com>
2013-02-12 21:38:55 -0600

    inKeys is now strict, same as inValues

--- src/Aura/Filter/Rule/InKeys.php
+++ src/Aura/Filter/Rule/InKeys.php
@@ -45,6 +45,10 @@ class InKeys extends AbstractRule
      * Given an array (second parameter), the value (first parameter) must 
      * match at least one of the array keys.
      * 
+     * Strict checking is enforced, so a string "1" is not the same as
+     * an integer 1.  This helps to avoid matching between 0, false, null,
+     * and empty string.
+     * 
      * @param array $array An array of key-value pairs; the value must match
      * one of the keys in this array.
      * 
@@ -53,8 +57,9 @@ class InKeys extends AbstractRule
      */
     public function validate(array $array)
     {
-        $this->setParams(['keys' => array_keys($array)]);
-        return array_key_exists($this->getValue(), $array);
+        $keys = array_keys($array);
+        $this->setParams(['keys' => $keys]);
+        return in_array($this->getValue(), $keys, true);
     }
 
     /**
