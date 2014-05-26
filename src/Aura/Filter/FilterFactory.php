71dba954803df49dabae86856ac1b26da4312d00
Luis Cordova <cordoval@gmail.com>
2014-05-26 12:34:15 -0500

    plug mock of factory

--- src/Aura/Filter/FilterFactory.php
+++ src/Aura/Filter/FilterFactory.php
@@ -1,1 +1,44 @@
-https://github.com/auraphp/Aura.View/blob/develop-2/src/ViewFactory.php
+<?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @package Aura.Filter
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
+namespace Aura\Filter;
+
+/**
+ *
+ * Factory to create Filter objects.
+ *
+ * @package Aura.Filter
+ *
+ */
+class FilterFactory
+{
+    /**
+     *
+     * Returns a new Filter instance.
+     *
+     * @param object $helpers An arbitrary helper manager for the Filter; if not
+     * specified, uses the HelperRegistry from this package.
+     *
+     * @return Filter
+     *
+     */
+    public function newInstance($helpers = null)
+    {
+        if (! $helpers) {
+            $helpers = new HelperRegistry;
+        }
+
+        return new Filter(
+            new TemplateRegistry,
+            new TemplateRegistry,
+            $helpers
+        );
+    }
+}
