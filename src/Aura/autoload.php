e34610adeb19ffd2c61ae96b347af9a105f852e5
Luis Cordova <cordoval@gmail.com>
2014-05-26 12:35:04 -0500

    fix autoload.php with right class name for filter

--- src/Aura/autoload.php
+++ src/Aura/autoload.php
@@ -2,7 +2,7 @@
 spl_autoload_register(function ($class) {
 
     // what namespace prefix should be recognized?
-    $prefix = 'Aura\View\\';
+    $prefix = 'Aura\Filter\\';
 
     // does the requested class match the namespace prefix?
     $prefix_len = strlen($prefix);
