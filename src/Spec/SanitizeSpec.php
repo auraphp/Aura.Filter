c9cb3250e54e561c1464d9217d7caca87b9914ac
Paul M. Jones <pmjones88@gmail.com>
2015-06-30 20:27:16 -0500

    update doblocks:

--- src/Spec/SanitizeSpec.php
+++ src/Spec/SanitizeSpec.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ *
+ * This file is part of Aura for PHP.
+ *
+ * @package Aura.Filter
+ *
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ *
+ */
 namespace Aura\Filter\Spec;
 
 use Aura\Filter\Rule\Locator\SanitizeLocator;
--- tests/unit/src/Rule/Validate/WordTest.php
+++ tests/unit/src/Rule/Validate/WordTest.php
@@ -1,15 +1,6 @@
 <?php
 namespace Aura\Filter\Rule\Validate;
 
-/**
- *
- * Sanitizes a value to a string with only word characters.
- *
- * @package Aura.Filter
- *
- * @license http://opensource.org/licenses/bsd-license.php BSD
- *
- */
 class WordTest extends AbstractValidateTest
 {
     public function providerIs()
