6052e4e4d7ce23d7559a1e0f6ba5de8d862e42cb
Paul M. Jones <pmjones88@gmail.com>
2015-07-02 16:42:06 -0500

    different testing structure now

--- autoload.php
+++ autoload.php
@@ -8,11 +8,9 @@ spl_autoload_register(function ($class) {
     $prefixes = array(
         "{$ns}\_Config\\" => array(
             __DIR__ . '/config',
-            __DIR__ . '/tests/container/src',
         ),
         "{$ns}\\" => array(
             __DIR__ . '/src',
-            __DIR__ . '/tests/unit/src',
         ),
     );
 
