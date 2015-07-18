e6bf5652577aff7a45dc56514936e62f5e09bdf5
Paul M. Jones <pmjones88@gmail.com>
2015-07-18 10:00:06 -0500

    add back ivanov_str_pad for 5.3 and 5.4

--- src/Rule/AbstractStrlen.php
+++ src/Rule/AbstractStrlen.php
@@ -65,13 +65,18 @@ abstract class AbstractStrlen
         return mb_substr($str, $start, $length);
     }
 
-    // http://php.net/manual/en/function.str-pad.php#111147
     protected function strpad($input, $pad_length, $pad_string = ' ', $pad_type = STR_PAD_RIGHT)
     {
         if (! function_exists('mb_strlen')) {
             return str_pad($input, $pad_length, $pad_string, $pad_type);
         }
 
+        return $this->ivanov_str_pad($input, $pad_length, $pad_string, $pad_type);
+    }
+
+    // http://php.net/manual/en/function.str-pad.php#111147
+    protected function mbstrpad($input, $pad_length, $pad_string = ' ', $pad_type = STR_PAD_RIGHT)
+    {
         $encoding = mb_internal_encoding();
         mb_internal_encoding('utf-8');
 
@@ -102,5 +107,33 @@ abstract class AbstractStrlen
 
         mb_internal_encoding($encoding);
         return $result;
+    }
+
+    protected function ivanov_str_pad($input, $length, $pad_str = " ", $type = STR_PAD_RIGHT, $encoding = "UTF-8") {
+        //return str_pad($str, strlen($str)-$this->strlen($str,$encoding)+$pad_length, $pad_string, $pad_type);
+
+        $input_len = $this->strlen($input,$encoding);
+        if ($length <= $input_len)
+            return $input;
+        $pad_str_len = $this->strlen($pad_str,$encoding);
+        $pad_len = $length - $input_len;
+        if ($type == STR_PAD_RIGHT) {
+            $repeat_times = ceil($pad_len / $pad_str_len);
+            return $this->substr($input . str_repeat($pad_str, $repeat_times), 0, $length,$encoding);
+        }
+        if ($type == STR_PAD_LEFT) {
+            $repeat_times = ceil($pad_len / $pad_str_len);
+            return $this->substr(str_repeat($pad_str, $repeat_times), 0, floor($pad_len),$encoding) . $input;
+        }
+        if ($type == STR_PAD_BOTH) {
+            $pad_len /= 2;
+            $pad_amount_left = floor($pad_len);
+            $pad_amount_right = ceil($pad_len);
+            $repeat_times_left = ceil($pad_amount_left / $pad_str_len);
+            $repeat_times_right = ceil($pad_amount_right / $pad_str_len);
+            $padding_left = $this->substr(str_repeat($pad_str, $repeat_times_left), 0, $pad_amount_left,$encoding);
+            $padding_right = $this->substr(str_repeat($pad_str, $repeat_times_right), 0, $pad_amount_right,$encoding);
+            return $padding_left . $input . $padding_right;
+        }
     }
 }
