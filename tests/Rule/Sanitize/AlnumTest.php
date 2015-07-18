40c66efed363643c09ab5a64d651b4632bf55eba
x0r <x0r@localhost.localdomain>
2015-07-18 21:37:22 +0300

    Created two files encoded by utf-16 LE(utf-16 is endian dependent, LE=Little endian). Testing with said files yields incorrect results.

--- tests/Rule/Sanitize/AlnumTest.php
+++ tests/Rule/Sanitize/AlnumTest.php
@@ -5,10 +5,12 @@ class AlnumTest extends AbstractSanitizeTest
 {
     public function providerTo()
     {
+        //print file_get_contents('../../StringsTest/UTF-16LE/Sanitize/alnum');
         return array(
             // value, result, expect
             array('$#% abc () 123 ,./', true, 'abc123'),
             array('$#% abc () 123 влц ,./', true, 'abc123влц'),
+            array(file_get_contents('./tests/MbStrings/UTF-16LE/alnum'), true, '123abcГБЦ')
         );
     }
 }
--- tests/Rule/Sanitize/AlphaTest.php
+++ tests/Rule/Sanitize/AlphaTest.php
@@ -1,13 +1,15 @@
 <?php
+
 namespace Aura\Filter\Rule\Sanitize;
 
-class AlphaTest extends AbstractSanitizeTest
-{
-    public function providerTo()
-    {
+class AlphaTest extends AbstractSanitizeTest {
+
+    public function providerTo() {
         return array(
             array('^&* abc 123 ,./', true, 'abc'),
             array('^&* abc гдб 123 ,./', true, 'abcгдб'),
+            array(file_get_contents('./tests/MbStrings/UTF-16LE/alpha'), true, 'abcгдб'),
         );
     }
+
 }
