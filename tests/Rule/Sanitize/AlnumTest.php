a3dc27fb055bca2b26a0ec31f24b48821975a6e7
x0r <x0r@localhost.localdomain>
2015-07-19 15:35:28 +0300

    Non-utf8 tests need additional files to compare the strings too. Not sure if the files I've generated are correct.

--- tests/Rule/Sanitize/AlnumTest.php
+++ tests/Rule/Sanitize/AlnumTest.php
@@ -10,7 +10,7 @@ class AlnumTest extends AbstractSanitizeTest
             // value, result, expect
             array('$#% abc () 123 ,./', true, 'abc123'),
             array('$#% abc () 123 влц ,./', true, 'abc123влц'),
-            array(file_get_contents('./tests/MbStrings/UTF-16LE/alnum'), true, '123abcГБЦ')
+            array(file_get_contents('./tests/MbStrings/UTF-16LE/alnum'), true, file_get_contents('./tests/MbStrings/UTF-16LE/alnum_true'))
         );
     }
 }
--- tests/Rule/Sanitize/AlphaTest.php
+++ tests/Rule/Sanitize/AlphaTest.php
@@ -8,7 +8,7 @@ class AlphaTest extends AbstractSanitizeTest {
         return array(
             array('^&* abc 123 ,./', true, 'abc'),
             array('^&* abc гдб 123 ,./', true, 'abcгдб'),
-            array(file_get_contents('./tests/MbStrings/UTF-16LE/alpha'), true, 'abcгдб'),
+            array(file_get_contents('./tests/MbStrings/UTF-16LE/alpha'), true,file_get_contents('./tests/MbStrings/UTF-16LE/alpha_true')),
         );
     }
 
