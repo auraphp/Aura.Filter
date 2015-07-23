47deefb860d5bd9696b5ed028975845dad5db702
Paul M. Jones <pmjones88@gmail.com>
2015-07-22 19:56:18 -0500

    diff cleanup

--- tests/Rule/Sanitize/StrTest.php
+++ tests/Rule/Sanitize/StrTest.php
@@ -14,6 +14,7 @@ class StrTest extends AbstractSanitizeTest
     public function providerTo()
     {
         return array(
+            array('abc 123 ,./', true, 'abc@123@,./'),
             array('abc 123 ,./ абв', true, 'abc@123@,./@абв'),
             array(12345, true, '12345'),
         );
